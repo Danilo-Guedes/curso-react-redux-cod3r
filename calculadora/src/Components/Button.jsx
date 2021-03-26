03e87da07c83c254351f8c4ace389b61fb6e52a2
Danilo Guedes <danilotiba@gmail.com>
2021-03-26 01:00:55 -0300

    creating Button component and adding css to it

---
+++ calculadora/src/Components/Button.jsx
@@ -0,0 +1,5 @@
+/* eslint-disable import/no-anonymous-default-export */
+import "./Button.css";
+import React from "react";
+
+export default (props) => <button className="button">{props.label}</button>;
