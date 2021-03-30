8678a72e178890c071c7558f4f5121f89947991c
Danilo Guedes <danilotiba@gmail.com>
2021-03-30 01:34:36 -0300

    creating navigation-router project base structure

---
+++ navegacao-router/src/views/examples/About.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const About = props => (
+    <div className='About'>
+       <h1>Sobre</h1>
+       <p>Nosso sistema foi criado ...</p>
+    </div>
+)
+
+export default About;
