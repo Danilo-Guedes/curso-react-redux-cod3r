eabaabe98c9d2b5ba5925436e3f5e50bd94b34df
Danilo Guedes <danilotiba@gmail.com>
2021-03-31 01:21:29 -0300

    solving even or odd challenge on my own

--- hooks/src/views/examples/UseEffect.jsx
+++ hooks/src/views/examples/UseEffect.jsx
@@ -1,38 +1,60 @@
-import React, { useEffect, useState } from 'react'
-import PageTitle from '../../components/layout/PageTitle'
-import SectionTitle from '../../components/layout/SectionTitle'
+import React, { useEffect, useState } from 'react';
+import PageTitle from '../../components/layout/PageTitle';
+import SectionTitle from '../../components/layout/SectionTitle';
 
-const calcSquared = n => n**2
+const calcSquared = n => n ** 2;
 
-const UseEffect = (props) => {
-    const [number, setNumber] = useState(0)
-    const [squared, setSquared] = useState(0)
-
-    useEffect(() => setSquared(calcSquared(number)),[number])
-
-    return (
-        <div className="UseEffect">
-            <PageTitle
-                title="Hook UseEffect"
-                subtitle="Permite executar efeitos colaterais em componentes funcionais!"
-            />
-            <SectionTitle title="Exercício #1"> </SectionTitle>
-            <div className="center">
-            <div>
-                <span className="text">Quadrado: </span>
-                <span className="text red">{squared}</span>
-            </div>
-                <input type="number" 
-                className="input" 
-                value={number} 
-                onChange={(e) => setNumber(e.target.value)}/>                
-            </div>
-            <SectionTitle title="Exercício #2"> </SectionTitle>
-            <div className="center">
-
-            </div>
-        </div>
-    )
+const isEven = n => {
+    if(n%2 === 0){
+        return true
+    } else{
+        return false
+    }
+    
 }
 
-export default UseEffect
+const UseEffect = props => {
+    const [number, setNumber] = useState(0);
+    const [squared, setSquared] = useState(0);
+
+    useEffect(() => setSquared(calcSquared(number)), [number]);
+
+    const [number2, setnumber2] = useState(0);
+    const [even, setEven] = useState(true)
+
+    useEffect(() => setEven(isEven(number2)),[number2])
+
+    return (
+        <div className='UseEffect'>
+            <PageTitle
+                title='Hook UseEffect'
+                subtitle='Permite executar efeitos colaterais em componentes funcionais!'
+            />
+            <SectionTitle title='Exercício #1'> </SectionTitle>
+            <div className='center'>
+                <div>
+                    <span className='text'>Quadrado: </span>
+                    <span className='text red'>{squared}</span>
+                </div>
+                <input
+                    type='number'
+                    className='input'
+                    value={number}
+                    onChange={e => setNumber(e.target.value)}
+                />
+            </div>
+            <SectionTitle title='Exercício #2'> </SectionTitle>
+            <div className='center'>
+                <input
+                    type='number'
+                    className='input'
+                    value={number2}
+                    onChange={e => setnumber2(e.target.value)}
+                />
+                <span className="text center">{even ? "par" : "impar"}</span>
+            </div>
+        </div>
+    );
+};
+
+export default UseEffect;
