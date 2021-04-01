2d926d87bdd1364a79a1154aaf4a4117b7abcb05
Danilo Guedes <danilotiba@gmail.com>
2021-04-01 00:43:53 -0300

    introduced to useRef - first exercise

--- hooks/src/views/examples/UseRef.jsx
+++ hooks/src/views/examples/UseRef.jsx
@@ -1,15 +1,38 @@
-import React from 'react'
-import PageTitle from '../../components/layout/PageTitle'
+import React, { useState, useRef, useEffect } from 'react';
+import PageTitle from '../../components/layout/PageTitle';
+import SectionTitle from '../../components/layout/SectionTitle';
 
-const UseRef = (props) => {
+const UseRef = props => {
+    const [value1, setValue1] = useState('');
+    const counter = useRef(0);
+
+    useEffect(
+        function () {
+            counter.current += 1;
+        },
+        [value1]
+    );
+
     return (
-        <div className="UseRef">
+        <div className='UseRef'>
             <PageTitle
-                title="Hook UseRef"
-                subtitle="Retorna um objeto mutável com a propriedade .current!"
+                title='Hook UseRef'
+                subtitle='Retorna um objeto mutável com a propriedade .current!'
+            />
+            <SectionTitle title='Exercício #1'></SectionTitle>
+            <div classname='center'>
+                <span className='text'>Mudado [</span>
+                <span className='text red'>{counter.current}</span>
+                <span className='text'>]</span>
+            </div>
+            <input
+                type='text'
+                className='input'
+                value={value1}
+                onChange={e => setValue1(e.target.value)}
             />
         </div>
-    )
-}
+    );
+};
 
-export default UseRef
+export default UseRef;
