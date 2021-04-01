f0c1d12734b208f19d03a8877650b64b41cba195
Danilo Guedes <danilotiba@gmail.com>
2021-04-01 02:26:35 -0300

    second useRef example + challenge

--- hooks/src/views/examples/UseRef.jsx
+++ hooks/src/views/examples/UseRef.jsx
@@ -2,15 +2,35 @@ import React, { useState, useRef, useEffect } from 'react';
 import PageTitle from '../../components/layout/PageTitle';
 import SectionTitle from '../../components/layout/SectionTitle';
 
+const merge = (v1, v2) => {
+    return [...v1].map((elem, index) => {
+        return `${elem}${v2[index] || ''}`
+    }).join('')
+};
+
 const UseRef = props => {
     const [value1, setValue1] = useState('');
-    const counter = useRef(0);
+    const [value2, setValue2] = useState('');
+    const counter = useRef(0); //retorna um obj mutável com propriedade <var>.current
+    //recebe uma propriedade direto do html chamada ref
+
+    const myInput1 = useRef(null);
+    const myInput2 = useRef(null);
+
+    useEffect(
+        function () {
+            counter.current++;
+            myInput2.current.focus();
+        },
+        [value1]
+    );
 
     useEffect(
         function () {
             counter.current += 1;
+            myInput1.current.focus();
         },
-        [value1]
+        [value2]
     );
 
     return (
@@ -20,16 +40,28 @@ const UseRef = props => {
                 subtitle='Retorna um objeto mutável com a propriedade .current!'
             />
             <SectionTitle title='Exercício #1'></SectionTitle>
-            <div classname='center'>
-                <span className='text'>Mudado [</span>
-                <span className='text red'>{counter.current}</span>
-                <span className='text'>]</span>
+            <div className='center'>
+                <div>
+                    <span className='text'>Valor: </span>
+                    <span className='text'>{merge(value1, value2)} [</span>
+                    <span className='text red'>{counter.current}</span>
+                    <span className='text'>]</span>
+                </div>
             </div>
             <input
                 type='text'
                 className='input'
                 value={value1}
+                ref={myInput1}
                 onChange={e => setValue1(e.target.value)}
+            />
+            <SectionTitle title='Exercício #2'></SectionTitle>
+            <input
+                type='text'
+                className='input'
+                valur={value2}
+                ref={myInput2}
+                onChange={e => setValue2(e.target.value)}
             />
         </div>
     );
