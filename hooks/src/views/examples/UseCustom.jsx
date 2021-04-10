c92a14894d20f177f211eea8d03033eca77b175f
Danilo Guedes <danilotiba@gmail.com>
2021-04-10 17:45:20 -0300

    creating my own hook

--- hooks/src/views/examples/UseCustom.jsx
+++ hooks/src/views/examples/UseCustom.jsx
@@ -1,15 +1,29 @@
 import React from 'react'
 import PageTitle from '../../components/layout/PageTitle'
+import SectionTitle from '../../components/layout/SectionTitle'
+import { useMyCounter } from '../../hooks/useMyCounter'
 
-const UseRef = (props) => {
+const UseCustom = (props) => {
+    const [count, inc, dec] = useMyCounter(10);
+
     return (
         <div className="UseCustom">
             <PageTitle
                 title="Seu Hook"
                 subtitle="Vamos aprender como criar o nosso próprio Hook!"
             />
+            <SectionTitle title="Exercício #01" />
+            
+            <div className="center">
+                <span className="text">{count}</span>
+                <div>
+                    <button className="btn" onClick={dec}>-1</button>
+                    <button className="btn" onClick={inc}>+1</button>
+                </div>
+            </div>
+
         </div>
     )
 }
 
-export default UseRef
+export default UseCustom
