6fa5ba9f3dc3edfe9f0b726dfb1805c5ca54c44f
Danilo Guedes <danilotiba@gmail.com>
2021-04-12 00:17:17 -0300

    creating Routes for navbar

--- todoapp/frontend/src/main/app.jsx
+++ todoapp/frontend/src/main/app.jsx
@@ -3,14 +3,13 @@ import 'modules/font-awesome/css/font-awesome.min.css'
 
 import React from 'react'
 
-import Todo from '../todo/todo'
-import About from '../about/about'
 import Menu from '../template/menu'
+import Routes from './routes'
 
 export default props => (
     <div className="container">
         <Menu />
-        <Todo />
-        <About />
+        <Routes />
+        
     </div>
 )
