28c711cbc97111ba1567eba084655627750f6045
Danilo Guedes <danilotiba@gmail.com>
2021-04-12 00:06:54 -0300

    creating firts components and navbar

--- todoapp/frontend/src/main/app.jsx
+++ todoapp/frontend/src/main/app.jsx
@@ -3,8 +3,14 @@ import 'modules/font-awesome/css/font-awesome.min.css'
 
 import React from 'react'
 
+import Todo from '../todo/todo'
+import About from '../about/about'
+import Menu from '../template/menu'
+
 export default props => (
     <div className="container">
-        <h1>Teste</h1>
+        <Menu />
+        <Todo />
+        <About />
     </div>
 )
---
+++ todoapp/frontend/src/todo/todo.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Todo extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Todo do Mestre</h1>
+            </div>
+        )
+    }
+}
