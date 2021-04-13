7675c4ef0337e803323f22ee34879e430711531a
Danilo Guedes <danilotiba@gmail.com>
2021-04-12 22:36:39 -0300

    creating If Components for conditional Components

--- todoapp/frontend/src/template/iconButton.jsx
+++ todoapp/frontend/src/template/iconButton.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 
-export default props => {
-    if (props.hide) {
-        return null;
-    } else {
-        return (
-            <button className={'btn btn-' + props.style} oClick={props.onClick}>
-                <i className={'fa fa-' + props.icon}></i>
-            </button>
-        );
-    }
-};
+import If from './if';
+
+export default props => (
+    <If test={!props.hide}>
+        <button className={'btn btn-' + props.style} oClick={props.onClick}>
+            <i className={'fa fa-' + props.icon}></i>
+        </button>
+    </If>
+);
