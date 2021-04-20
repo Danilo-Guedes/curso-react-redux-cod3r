a9c13c66e13bcb770f0a21cdcd5964cf52289512
Danilo Guedes <danilotiba@gmail.com>
2021-04-19 23:56:26 -0300

    created search action and refactored todoForm as a class based Component

--- redux-todoapp/frontend/src/todo/todoForm.jsx
+++ redux-todoapp/frontend/src/todo/todoForm.jsx
@@ -1,59 +1,70 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux'
+import { bindActionCreators } from 'redux';
 
 import Grid from '../template/grid';
 import IconButton from '../template/iconButton';
-import { changeDescription } from './todoActions';
+import { changeDescription, search } from './todoActions';
 
-const TodoForm = props => {
-    const keyHandler = (e) => {
-        if(e.key === "Enter") {
-            e.shiftKey ? props.handleSearch() : props.handleAdd()
-        } else if(e.key === "Escape") {
-            props.handleClear()
+class TodoForm extends Component {
+    constructor(props) {
+        super(props);
+        this.keyHandler = this.keyHandler.bind(this);
+    }
+
+    componentWillMount(){
+        this.props.search()
+    }
+
+    keyHandler(e) {
+        if (e.key === 'Enter') {
+            e.shiftKey ? this.props.handleSearch() : this.props.handleAdd();
+        } else if (e.key === 'Escape') {
+            this.props.handleClear();
         }
     }
-    return (
-        <div role='form' className='todoForm'>
-            <Grid cols='12 9 10'>
-                <input
-                    id='description'
-                    type='text'
-                    className='form-control'
-                    placeholder='Adicione uma tarefa'
-                    value={props.description}
-                    onChange={props.changeDescription}
-                    onKeyUp={keyHandler}
-                ></input>
-            </Grid>
-            <Grid cols='12 3 2 '>
-                <IconButton
-                    style='primary'
-                    icon='plus'
-                    onClick={props.handleAdd}
-                />
-                <IconButton
-                    style='info'
-                    icon='search'
-                    onClick={props.handleSearch}
-                />
-                <IconButton
-                    style='default'
-                    icon='close'
-                    onClick={props.handleClear}
-                />
-            </Grid>
-        </div>
-    );
-};
+    render() {
+        return (
+            <div role='form' className='todoForm'>
+                <Grid cols='12 9 10'>
+                    <input
+                        id='description'
+                        type='text'
+                        className='form-control'
+                        placeholder='Adicione uma tarefa'
+                        value={this.props.description}
+                        onChange={this.props.changeDescription}
+                        onKeyUp={this.keyHandler}
+                    ></input>
+                </Grid>
+                <Grid cols='12 3 2 '>
+                    <IconButton
+                        style='primary'
+                        icon='plus'
+                        onClick={this.props.handleAdd}
+                    />
+                    <IconButton
+                        style='info'
+                        icon='search'
+                        onClick={this.props.handleSearch}
+                    />
+                    <IconButton
+                        style='default'
+                        icon='close'
+                        onClick={this.props.handleClear}
+                    />
+                </Grid>
+            </div>
+        );
+    }
+}
 
-const mapStateToProps = (state) => ({
-    description: state.todo.description
-})
+const mapStateToProps = state => ({
+    description: state.todo.description,
+});
 
 const mapDispatchToProps = dispatch => {
-    return bindActionCreators({ changeDescription }, dispatch)
-}
+    return bindActionCreators({ changeDescription, search }, dispatch);
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(TodoForm);
--- redux-todoapp/frontend/src/todo/todoReducer.js
+++ redux-todoapp/frontend/src/todo/todoReducer.js
@@ -1,28 +1,31 @@
 const INITIAL_STATE = {
-    
-        description: 'Ler Livro',
-        list: [{
+    description: 'Ler Livro',
+    list: [
+        {
             _id: 1,
             description: 'Pagar Fatura do Cartão',
-            done: true
-        }, {
+            done: true,
+        },
+        {
             _id: 2,
             description: 'Jogar Csgo',
-            done: false
-        }, {
+            done: false,
+        },
+        {
             _id: 3,
             description: 'Estudar React',
-            done: false
-        }]
-    
-}
+            done: false,
+        },
+    ],
+};
 
 export default (state = INITIAL_STATE, action) => {
-    switch(action.type){
+    switch (action.type) {
         case 'DESCRIPTION_CHANGED':
-            return {...state, description: action.payload}
+            return { ...state, description: action.payload };
+        case 'TODO_SEARCHED':
+            return { ...state, list: action.payload.data };
         default:
-            return state
+            return state;
     }
-}
-
+};
