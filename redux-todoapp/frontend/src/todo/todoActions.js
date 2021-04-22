7d5859947180d3aef1924eed5761ff97af6789d6
Danilo Guedes <danilotiba@gmail.com>
2021-04-21 23:28:52 -0300

    created add action but still not working properly

--- redux-todoapp/frontend/src/todo/todoActions.js
+++ redux-todoapp/frontend/src/todo/todoActions.js
@@ -15,3 +15,12 @@ export const search = () => {
         payload: request
     }
 }
+
+export const add = (description) => {
+    const request = axios.post(URL, {description});
+
+    return {
+        type: 'TODO_ADDED',
+        payload: request
+    }
+} 
--- redux-todoapp/frontend/src/todo/todoForm.jsx
+++ redux-todoapp/frontend/src/todo/todoForm.jsx
@@ -4,7 +4,7 @@ import { bindActionCreators } from 'redux';
 
 import Grid from '../template/grid';
 import IconButton from '../template/iconButton';
-import { changeDescription, search } from './todoActions';
+import { changeDescription, search, add } from './todoActions';
 
 class TodoForm extends Component {
     constructor(props) {
@@ -17,13 +17,15 @@ class TodoForm extends Component {
     }
 
     keyHandler(e) {
+        const { add, search, description } = this.props
         if (e.key === 'Enter') {
-            e.shiftKey ? this.props.handleSearch() : this.props.handleAdd();
+            e.shiftKey ? search() : add(description);
         } else if (e.key === 'Escape') {
             this.props.handleClear();
         }
     }
     render() {
+        const { add, search, description } = this.props
         return (
             <div role='form' className='todoForm'>
                 <Grid cols='12 9 10'>
@@ -41,12 +43,12 @@ class TodoForm extends Component {
                     <IconButton
                         style='primary'
                         icon='plus'
-                        onClick={this.props.handleAdd}
+                        onClick={() => add(description)}
                     />
                     <IconButton
                         style='info'
                         icon='search'
-                        onClick={this.props.handleSearch}
+                        onClick={() => search()}
                     />
                     <IconButton
                         style='default'
@@ -64,7 +66,7 @@ const mapStateToProps = state => ({
 });
 
 const mapDispatchToProps = dispatch => {
-    return bindActionCreators({ changeDescription, search }, dispatch);
+    return bindActionCreators({ changeDescription, search, add}, dispatch);
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(TodoForm);
