d629c04b8b9bdb7471dbc160f42307983a1998ac
Danilo Guedes <danilotiba@gmail.com>
2021-04-21 23:42:19 -0300

    using redux-multi middleware for passing more then one task inside a array in add action creator

--- redux-todoapp/frontend/src/todo/todoActions.js
+++ redux-todoapp/frontend/src/todo/todoActions.js
@@ -1,26 +1,33 @@
 import axios from 'axios';
 
-const URL = 'http://localhost:3003/api/todos'
+const URL = 'http://localhost:3003/api/todos';
 
 export const changeDescription = event => ({
     type: 'DESCRIPTION_CHANGED',
-    payload: event.target.value
-}) 
+    payload: event.target.value,
+});
 
 export const search = () => {
     const request = axios.get(`${URL}?sort=-createdAt`);
 
     return {
         type: 'TODO_SEARCHED',
-        payload: request
-    }
-}
+        payload: request,
+    };
+};
 
-export const add = (description) => {
-    const request = axios.post(URL, {description});
+// JEITO ANTIGO PORÉM NÃO PERMITE CHAMAR O SEARCH DEPOIS DA CRIAÇÃO DO TODO
+// export const add = (description) => {
+//     const request = axios.post(URL, {description});
 
-    return {
-        type: 'TODO_ADDED',
-        payload: request
-    }
-} 
+//     return {
+//         type: 'TODO_ADDED',
+//         payload: request
+//     }
+// }
+
+export const add = description => {
+    const request = axios.post(URL, { description });
+
+    return [{ type: 'TODO_ADDED', payload: request }, search()];
+};
