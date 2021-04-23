e2ce0a84a25dd85327a92bb9494afb245b6396de
Danilo Guedes <danilotiba@gmail.com>
2021-04-23 00:42:45 -0300

    configuring api and billingCycle routes

---
+++ my-money-app/backend/src/config/routes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+module.exports = function (server) {
+    // definindo URL BASE para todas as rotas
+    const router = express.Router();
+
+    router.use('/api', router);
+
+    //definindo as rotas do CICLO DE PAGAMENTOS
+    const BillingCycle = require('../api/billingCycle/billingCycleServices');
+    BillingCycle.register(router, '/billingCycles');
+};
