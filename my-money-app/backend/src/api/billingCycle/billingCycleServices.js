719e12e440a0476e58a0cf0fa19336a0246d8f75
Danilo Guedes <danilotiba@gmail.com>
2021-04-25 21:57:48 -0300

    created the summary route/method that return the sum of credits and debts of the db using mongoose Aggregate method

--- my-money-app/backend/src/api/billingCycle/billingCycleServices.js
+++ my-money-app/backend/src/api/billingCycle/billingCycleServices.js
@@ -23,4 +23,26 @@ BillingCycle.route('count', (req, res, next) => {
     });
 });
 
+BillingCycle.route('summary', (req, res, next) => {
+    BillingCycle.aggregate({
+        $project: {credit: {$sum: "$credits.value"}, debt: {$sum: "$debts.value"}}, // RETORNA UMA LISTA COM A SOMA DE CADA MÊS
+        
+    }, {
+    $group: {_id: null, credit: {$sum: "$credit"}, debt: {$sum: "$debt"}} // AGRUPA A LISTA EM UM UNICO VALOR DE CREDIT E DEBT, TIPO UM REDUCE
+    }, {
+     $project: {_id: 0, credit: 1, debt: 1} // SETTA O ID COMO FALSE E OS OUTROS ITENS COMO TRUE
+     // $project: {_id: false, credit: true, debt: true}
+    },
+    (error, result) => { //TRATA O ERRO
+        if(error){
+            res.status(500).json({errors: [error]})
+        } else {
+            res.json(result || {credit: 0, debit: 0})
+        }
+    }
+
+    )
+
+})
+
 module.exports = BillingCycle;
