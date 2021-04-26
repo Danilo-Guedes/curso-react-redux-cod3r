3453c8e86754edda1d6f1d2a4e20fd5a438ccbd5
Danilo Guedes <danilotiba@gmail.com>
2021-04-25 23:24:59 -0300

    setting up express-query-int middleware

--- my-money-app/backend/src/config/server.js
+++ my-money-app/backend/src/config/server.js
@@ -2,13 +2,15 @@ const PORT = 3003;
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const allowCors = require('./cors')
+const allowCors = require('./cors');
+const queryParser = require('express-query-int')
 const server = express();
 
 
 server.use(bodyParser.urlencoded({ extended: true }));
 server.use(bodyParser.json());
 server.use(allowCors);
+server.use(queryParser())
 
 server.listen(PORT, () =>
     console.log(`Server is runnig at http://localhost:${PORT}`)
