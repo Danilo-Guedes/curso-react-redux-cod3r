9167ec90a5cbf051d28a4696d4c3e959edac63b9
Danilo Guedes <danilotiba@gmail.com>
2021-04-25 23:13:32 -0300

    CORS configured

--- my-money-app/backend/src/config/server.js
+++ my-money-app/backend/src/config/server.js
@@ -1,11 +1,14 @@
 const PORT = 3003;
+
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const allowCors = require('./cors')
 const server = express();
+
 
 server.use(bodyParser.urlencoded({ extended: true }));
 server.use(bodyParser.json());
+server.use(allowCors);
 
 server.listen(PORT, () =>
     console.log(`Server is runnig at http://localhost:${PORT}`)
