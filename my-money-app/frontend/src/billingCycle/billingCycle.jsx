3545c9c2534f3c27dbbffac7650453b31d56aeef
Danilo Guedes <danilotiba@gmail.com>
2021-04-29 23:04:50 -0300

    created tabHeader Component

--- my-money-app/frontend/src/billingCycle/billingCycle.jsx
+++ my-money-app/frontend/src/billingCycle/billingCycle.jsx
@@ -5,20 +5,41 @@ import Content from '../common/template/content'
 import Tabs from '../common/tab/tabs'
 import TabsContent from '../common/tab/tabsContent'
 import TabsHeader from '../common/tab/tabsHeader'
+import TabHeader from '../common/tab/tabHeader'
 
 class BillingCycle extends Component {
     render() {
         return (
             <div>
-                <ContentHeader title='Ciclos de Pagamentos' subtitle="Cadastro" />
+                <ContentHeader
+                    title='Ciclos de Pagamentos'
+                    subtitle='Cadastro'
+                />
                 <Content>
                     <Tabs>
                         <TabsHeader>
-
+                            <TabHeader
+                                label='Listar'
+                                icon='bars'
+                                target='tabList'
+                            />
+                            <TabHeader
+                                label='Incluir'
+                                icon='plus'
+                                target='tabCreate'
+                            />
+                            <TabHeader
+                                label='Alterar'
+                                icon='pencil'
+                                target='tabUpdate'
+                            />
+                            <TabHeader
+                                label='Excluir'
+                                icon='trash-o'
+                                target='tabDelete'
+                            />
                         </TabsHeader>
-                        <TabsContent>
-                            
-                        </TabsContent>
+                        <TabsContent></TabsContent>
                     </Tabs>
                 </Content>
             </div>
---
+++ my-money-app/frontend/src/common/tab/tabHeader.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+class TabHeader extends Component {
+    render() {
+        return (
+            <li>
+                <a
+                    href='javascript:;'
+                    tab-toggle='tab'
+                    data-target={this.props.target}
+                >
+                    <i className={`fa fa-${this.props.icon}`}></i>
+                    {this.props.label}
+                </a>
+            </li>
+        )
+    }
+}
+
+export default TabHeader
