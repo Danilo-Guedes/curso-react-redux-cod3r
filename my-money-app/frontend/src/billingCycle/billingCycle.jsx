534cfc4a3bd03025de1c441afc802fb62313b3b8
Danilo Guedes <danilotiba@gmail.com>
2021-04-29 22:40:44 -0300

    created Tabs TabsHeader and TabsContent Components

--- my-money-app/frontend/src/billingCycle/billingCycle.jsx
+++ my-money-app/frontend/src/billingCycle/billingCycle.jsx
@@ -2,6 +2,9 @@ import React, { Component } from 'react'
 
 import ContentHeader from '../common/template/contentHeader'
 import Content from '../common/template/content'
+import Tabs from '../common/tab/tabs'
+import TabsContent from '../common/tab/tabsContent'
+import TabsHeader from '../common/tab/tabsHeader'
 
 class BillingCycle extends Component {
     render() {
@@ -9,7 +12,14 @@ class BillingCycle extends Component {
             <div>
                 <ContentHeader title='Ciclos de Pagamentos' subtitle="Cadastro" />
                 <Content>
-                    Ciclo de Pagamentos
+                    <Tabs>
+                        <TabsHeader>
+
+                        </TabsHeader>
+                        <TabsContent>
+                            
+                        </TabsContent>
+                    </Tabs>
                 </Content>
             </div>
         )
