29e7daa74588d8fa876a45080685888bf524342e
Danilo Guedes <danilotiba@gmail.com>
2021-04-30 00:54:58 -0300

    configured standard tab selected when open billigCycle Component

--- my-money-app/frontend/src/billingCycle/billingCycle.jsx
+++ my-money-app/frontend/src/billingCycle/billingCycle.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
 
 import ContentHeader from '../common/template/contentHeader'
 import Content from '../common/template/content'
@@ -7,8 +9,12 @@ import TabsContent from '../common/tab/tabsContent'
 import TabsHeader from '../common/tab/tabsHeader'
 import TabHeader from '../common/tab/tabHeader'
 import TabContent from '../common/tab/tabContent'
+import { selectTab } from '../common/tab/tabActions'
 
 class BillingCycle extends Component {
+    componentWillMount() {
+        this.props.selectTab('tabList') // SELECIONANDO A ABA PADRÃO
+    }
     render() {
         return (
             <div>
@@ -41,10 +47,18 @@ class BillingCycle extends Component {
                             />
                         </TabsHeader>
                         <TabsContent>
-                            <TabContent id='tabList'><div>Listar</div></TabContent>
-                            <TabContent id='tabCreate'><div>Incluir</div></TabContent>
-                            <TabContent id='tabUpdate'><div>Alterar</div></TabContent>
-                            <TabContent id='tabDelete'><div>Excluir</div></TabContent>
+                            <TabContent id='tabList'>
+                                <h1>Listar</h1>
+                            </TabContent>
+                            <TabContent id='tabCreate'>
+                                <h1>Incluir</h1>
+                            </TabContent>
+                            <TabContent id='tabUpdate'>
+                                <h1>Alterar</h1>
+                            </TabContent>
+                            <TabContent id='tabDelete'>
+                                <h1>Excluir</h1>
+                            </TabContent>
                         </TabsContent>
                     </Tabs>
                 </Content>
@@ -53,4 +67,8 @@ class BillingCycle extends Component {
     }
 }
 
-export default BillingCycle
+
+const mapDispatchToProps = dispatch =>
+    bindActionCreators({ selectTab }, dispatch)
+
+export default connect(null, mapDispatchToProps)(BillingCycle)
