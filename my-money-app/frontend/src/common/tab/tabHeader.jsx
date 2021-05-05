7fa85a25d6507e1af0fa84fda526348a00e03c0e
Danilo Guedes <danilotiba@gmail.com>
2021-05-04 23:05:42 -0300

    created If Component to handle tabs rendering

--- my-money-app/frontend/src/common/tab/tabHeader.jsx
+++ my-money-app/frontend/src/common/tab/tabHeader.jsx
@@ -4,22 +4,26 @@ import { connect } from 'react-redux'
 
 import { selectTab } from './tabActions'
 
-class TabHeader extends Component {
+import If from '../operator/if'
 
+class TabHeader extends Component {
     render() {
         const isSelected = this.props.tab.selected === this.props.target
+        const visible = this.props.tab.visible[this.props.target]
         return (
-            <li className={isSelected ? 'active' : ''}>
-                <a
-                    href='javascript:;'
-                    data-toggle='tab'
-                    data-target={this.props.target}
-                    onClick={() => this.props.selectTab(this.props.target)}
-                >
-                    <i className={`fa fa-${this.props.icon}`}></i>
-                    {this.props.label}
-                </a>
-            </li>
+            <If test={visible}>
+                <li className={isSelected ? 'active' : ''}>
+                    <a
+                        href='javascript:;'
+                        data-toggle='tab'
+                        data-target={this.props.target}
+                        onClick={() => this.props.selectTab(this.props.target)}
+                    >
+                        <i className={`fa fa-${this.props.icon}`}></i>
+                        {this.props.label}
+                    </a>
+                </li>
+            </If>
         )
     }
 }
@@ -27,6 +31,5 @@ class TabHeader extends Component {
 const mapStateToProps = state => ({ tab: state.tab })
 const mapDispatchToProps = dispatch =>
     bindActionCreators({ selectTab }, dispatch)
-
 
 export default connect(mapStateToProps, mapDispatchToProps)(TabHeader)
