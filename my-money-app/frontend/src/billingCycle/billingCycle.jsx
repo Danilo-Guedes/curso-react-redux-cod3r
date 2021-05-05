5eb4bb76e2a76bbc49dec58ff482f028921408e8
Danilo Guedes <danilotiba@gmail.com>
2021-05-04 22:45:14 -0300

    created showTabs action and reducer to manage visibleTabs in billingCycle Component.

--- my-money-app/frontend/src/billingCycle/billingCycle.jsx
+++ my-money-app/frontend/src/billingCycle/billingCycle.jsx
@@ -9,11 +9,12 @@ import TabsContent from '../common/tab/tabsContent'
 import TabsHeader from '../common/tab/tabsHeader'
 import TabHeader from '../common/tab/tabHeader'
 import TabContent from '../common/tab/tabContent'
-import { selectTab } from '../common/tab/tabActions'
+import { selectTab, showTabs } from '../common/tab/tabActions'
 
 class BillingCycle extends Component {
     componentWillMount() {
         this.props.selectTab('tabList') // SELECIONANDO A ABA PADRÃO
+        this.props.showTabs('tabList', 'tabCreate')
     }
     render() {
         return (
@@ -69,6 +70,6 @@ class BillingCycle extends Component {
 
 
 const mapDispatchToProps = dispatch =>
-    bindActionCreators({ selectTab }, dispatch)
+    bindActionCreators({ selectTab, showTabs }, dispatch)
 
 export default connect(null, mapDispatchToProps)(BillingCycle)
