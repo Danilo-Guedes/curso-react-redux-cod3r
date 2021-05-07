2bd30bc6b0dee1b0491e21c7573dc049ad75719d
Danilo Guedes <danilotiba@gmail.com>
2021-05-07 00:48:30 -0300

    refactored create action to dispatch resetForm getList and selectTab managed by redux-multi middleware

--- my-money-app/frontend/src/billingCycle/billingCycleActions.js
+++ my-money-app/frontend/src/billingCycle/billingCycleActions.js
@@ -1,28 +1,40 @@
 import axios from 'axios'
 import { toastr } from 'react-redux-toastr'
+import {reset as resetForm} from 'redux-form' 
+import {showTabs, selectTab} from '../common/tab/tabActions'
+
 const URL = 'http://localhost:3003/api'
 
 export const getList = () => {
     const request = axios.get(`${URL}/billingCycles`)
     return {
         type: 'BILLING_CYCLES_FETCHED',
-        payload: request
+        payload: request,
     }
 }
 
-export function create(values){
+export function create(values) {
     // const texto = console.log(values);
-    axios.post(`${URL}/billingCycles`, values)
-    .then(resp => {
-        toastr.success('Sucesso', 'Operação realizada com sucesso!')
-    })
-    .catch(e =>{
-        e.response.data.errors.forEach(erro => {
-            toastr.error('Erro', erro)
-        })
-    })
-    return {
-        type: 'TEMP'   
-        
+
+    return dispatch => {
+        axios
+            .post(`${URL}/billingCycles`, values)
+            .then(resp => {
+                toastr.success('Sucesso', 'Operação realizada com sucesso!');
+                dispatch([
+                    resetForm('billingCycleForm'),
+                    getList(),
+                    selectTab('tabList'),
+                    showTabs('tabList', 'tabCreate') //a principio não está mudando nada, somente para uso futuro
+                ])
+             
+            })
+            
+            
+            .catch(e => {
+                e.response.data.errors.forEach(erro => {
+                    toastr.error('Erro', erro)
+                })
+            })
     }
 }
